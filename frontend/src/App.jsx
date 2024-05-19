55f7b23c732de89eba883eaf9b850f90a30a2560
ABHISHEK M B <117739148+MBABHISHEK@users.noreply.github.com>
2024-05-19 17:39:06 +0530

    Update App.jsx
    the path is changed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,96 +1,90 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import { Outlet } from 'react-router-dom'
-import PrivateRoute from "./components/Routing/PrivateRoute"
-import Home from "./components/GeneralScreens/Home"
-import LoginScreen from "./components/AuthScreens/LoginScreen"
-import RegisterScreen from "./components/AuthScreens/RegisterScreen"
-import ForgotPasswordScreen from "./components/AuthScreens/ForgotPasswordScreen"
-import ResetPasswordScreen from "./components/AuthScreens/ResetPasswordScreen"
-import Header from "./components/GeneralScreens/Header"
-import Footer from "./components/GeneralScreens/Footer"
-import Profile from "./components/ProfileScreens/Profile"
-import EditProfile from './components/ProfileScreens/EditProfile'
-import ChangePassword from './components/ProfileScreens/ChangePassword'
-import NotFound from './components/GeneralScreens/NotFound'
-import AddQuestion from "./components/QuestionScreens/AddQuestion"
-import DetailQuestion from "./components/QuestionScreens/DetailQuestion"
-import EditQuestion from "./components/QuestionScreens/EditQuestion"
-import AuthContextProvider from './context/AuthContext'
-
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
+import PrivateRoute from "./components/Routing/PrivateRoute";
+import Home from "./components/GeneralScreens/Home";
+import LoginScreen from "./components/AuthScreens/LoginScreen";
+import RegisterScreen from "./components/AuthScreens/RegisterScreen";
+import ForgotPasswordScreen from "./components/AuthScreens/ForgotPasswordScreen";
+import ResetPasswordScreen from "./components/AuthScreens/ResetPasswordScreen";
+import Header from "./components/GeneralScreens/Header";
+import Footer from "./components/GeneralScreens/Footer";
+import Profile from "./components/ProfileScreens/Profile";
+import EditProfile from './components/ProfileScreens/EditProfile';
+import ChangePassword from './components/ProfileScreens/ChangePassword';
+import NotFound from './components/GeneralScreens/NotFound';
+import AddQuestion from "./components/QuestionScreens/AddQuestion";
+import DetailQuestion from "./components/QuestionScreens/DetailQuestion";
+import EditQuestion from "./components/QuestionScreens/EditQuestion";
+import AuthContextProvider from './context/AuthContext';
 
 function App() {
-
   return (
     <AuthContextProvider>
-    <Router>
-      <div className='App'>
-        <Routes>
+      <Router>
+        <div className='App'>
+          <Routes>
             <Route path="/" element={<LayoutsWithHeader />}>
-                <Route path='*' element={<NotFound />}/>
+              <Route path="*" element={<NotFound />} />
+              
+              <Route path="/" element={<PrivateRoute />}>
+                <Route index element={<Home />} />
+              </Route>
 
-                <Route exact path='/' element={<PrivateRoute />}>
-                    <Route exact path='/' element={<Home />} />
-                </Route>
+              <Route path="/question/:slug" element={<DetailQuestion />} />
 
-                <Route exact path="question/:slug" element={<DetailQuestion />} />
+              <Route path="/addquestion" element={<PrivateRoute />}>
+                <Route index element={<AddQuestion />} />
+              </Route>
 
-                {/* <Route exact path='addquestion' element={<PrivateRoute />}>
-                </Route> */}
-                <Route exact path='addquestion' element={<AddQuestion/>} />
+              <Route path="/profile" element={<PrivateRoute />}>
+                <Route index element={<Profile />} />
+              </Route>
 
-                <Route exact path='profile' element={<PrivateRoute />}>
-                    <Route exact path='profile' element={<Profile />} />
-                </Route>
+              <Route path="/edit_profile" element={<PrivateRoute />}>
+                <Route index element={<EditProfile />} />
+              </Route>
 
-                <Route exact path='edit_profile' element={<PrivateRoute />}>
-                  <Route exact path='edit_profile' element={<EditProfile />} />
-                </Route>
+              <Route path="/change_password" element={<PrivateRoute />}>
+                <Route index element={<ChangePassword />} />
+              </Route>
 
-                <Route exact path='change_Password' element={<PrivateRoute />}>
-                      <Route exact path='change_Password' element={<ChangePassword />} />
-                </Route>
+              <Route path="/question/:slug/like" element={<PrivateRoute />}>
+                <Route index element={<DetailQuestion />} />
+              </Route>
 
-                <Route exact path='question/:slug/like' element={<PrivateRoute />}>
-                    <Route exact path='question/:slug/like' element={<DetailQuestion />} />
-                </Route>
+              <Route path="/question/:slug/edit" element={<PrivateRoute />}>
+                <Route index element={<EditQuestion />} />
+              </Route>
 
-                <Route exact path='question/:slug/edit' element={<PrivateRoute />}>
-                    <Route exact path='question/:slug/edit' element={<EditQuestion />} />
-                </Route>
+              <Route path="/question/:slug/delete" element={<PrivateRoute />}>
+                <Route index element={<DetailQuestion />} />
+              </Route>
 
-                <Route exact path='question/:slug/delete' element={<PrivateRoute />}>
-                    <Route exact path='question/:slug/delete' element={<DetailQuestion />} />
-                </Route>
-
-                <Route exact path='question/:slug/addQuestion' element={<PrivateRoute />}>
-                    <Route exact path='question/:slug/addAnswer' element={<DetailQuestion />} />
-                </Route>
+              <Route path="/question/:slug/addAnswer" element={<PrivateRoute />}>
+                <Route index element={<DetailQuestion />} />
+              </Route>
 
             </Route>
 
-            <Route exact path="login" element={<LoginScreen />} />
-
-            <Route exact path="register" element={<RegisterScreen />} />
-
-            <Route exact path="forgotpassword" element={<ForgotPasswordScreen />} />
-
-            <Route exact path="resetpassword" element={<ResetPasswordScreen />} />
-        </Routes>
-      </div>
-    </Router>
+            <Route path="/login" element={<LoginScreen />} />
+            <Route path="/register" element={<RegisterScreen />} />
+            <Route path="/forgotpassword" element={<ForgotPasswordScreen />} />
+            <Route path="/resetpassword" element={<ResetPasswordScreen />} />
+          </Routes>
+        </div>
+      </Router>
     </AuthContextProvider>
-  )
+  );
 }
 
 const LayoutsWithHeader = () => {
   return (
-        <>
-              <Header />
-              <Outlet />
-              <Footer />
-        </>
+    <>
+      <Header />
+      <Outlet />
+      <Footer />
+    </>
   );
 }
 
-export default App
+export default App;
